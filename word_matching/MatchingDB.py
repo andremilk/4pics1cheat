433826df4000bf71f48ac1f3e8c51b21d2d77741
Andre Leite <andre.guimaraes.leite@gmail.com>
2015-12-26 12:09:41 +0000

    Adds strategy to match for words on a database

---
+++ word_matching/MatchingDB.py
@@ -0,0 +1,13 @@
+import MySQLdb
+
+class MatchingDB:
+    def __init__(self, host, user, passwd, db, table, column):
+        self.conn = MySQLdb.connect(host=host, user=user, passwd=passwd, db=db)
+        self.table = table
+        self.db = db
+        self.column = column
+        self.cursor = self.conn.cursor()
+
+    def word_exists(self, word):
+        sql = "SELECT {0} FROM {1} where {1} = '{2}'".format(self.db, self.column, word)
+        return bool(self.cursor.execute(sql))
