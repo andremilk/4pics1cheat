78d139732e3c4d5118ab1a0bd58698c75d7b35fb
Andre Leite <andre.guimaraes.leite@gmail.com>
2015-12-26 16:04:36 +0000

    Adds handler to KeyboardInterrupt

--- cheat.py
+++ cheat.py
@@ -1,24 +1,28 @@
 from word_matching import MatchingDB
+import sys
 
 def main():
     mdb = MatchingDB.MatchingDB('172.17.0.2', 'root', 'root', 'word', 'word', 'word')
     # TODO: select matcher 
-    # TODO: keyboard interrupt gracefully interrupting
-    while True:
-        number_of_chars, pattern = input("Number of characters and pattern :").split(' ')
-        possible_solutions = []
-        results = set()
-        for word in mdb.get_words(int(number_of_chars)):
-            new_pattern = pattern
-            for c in word[1]:
-                if c not in new_pattern: 
-                    break
+    try:
+        while True:
+            number_of_chars, pattern = input("Number of characters and pattern :").split(' ')
+            possible_solutions = []
+            results = set()
+            for word in mdb.get_words(int(number_of_chars)):
+                new_pattern = pattern
+                for c in word[1]:
+                    if c not in new_pattern: 
+                        break
+                    else:
+                        new_pattern = new_pattern.replace(c, "", 1)
                 else:
-                    new_pattern = new_pattern.replace(c, "", 1)
-            else:
-                results.add(word[1])
+                    results.add(word[1])
 
-        print(sorted(results))
+            print(sorted(results))
+    except KeyboardInterrupt:
+        print('')
+        sys.exit(0)
 
 
 
